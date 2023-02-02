8de7511f63cc9727ef9ad47a9406b5b8ac68ed6e
Aliripoll <aripollortega@gmail.com>
2023-02-02 13:46:21 +0100

    add ampliation kata 10

--- kata_10/index.js
+++ kata_10/index.js
@@ -1,7 +1,7 @@
 /* Tienes un array de strings que contienen diferentes palabras, tu reto es retornar un array solo con las que cumplan con la condición de tener 4 o más letras.
 Para solucionarlo vas a encontrar una función llamada filterByLength que recibe un parámetro de entrada:
 
-array: Un array de strigs con palabras. Dentro del cuerpo de la función filterByLength debes escribir tu solución.
+array: Un array de strings con palabras. Dentro del cuerpo de la función filterByLength debes escribir tu solución.
 
 Ejemplo 1:
 
@@ -12,10 +12,23 @@ Ejemplo 2:
 Input: ['rosa', 'gol', 'pez', 'día', 'gafas'] Output: [ 'rosa', 'gafas' ] */
 
 let names = ['amor', 'sol', 'piedra', 'día'];
-let names2 = ['rosa', 'gol', 'pez', 'día', 'gafas'];
+let names2 = ['rosa', 'gol', 'pez', 'día', 'gafas',];
 
 function filterByLength(array) {
     return array.filter(element => element.length >= 4);
 }
 filterByLength(names);
 filterByLength(names2);
+
+
+/* AMPLIACIÓN 
+Además debe contener la vocal "i" */
+
+let nam = ['amor', 'sol', 'piedra', 'día'];
+let nam2 = ['rosa', 'gol', 'pez', 'día', 'gafas', "silla"];
+
+function filterByLength2(array) {
+    return array.filter(element => element.length >= 4 && element.includes("i"));
+}
+filterByLength2(nam);
+filterByLength2(nam2);
